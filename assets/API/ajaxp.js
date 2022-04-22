09e0cf5c81f15f61ed817257dc2b8b204d53762e
kele <984739634@qq.com>
2022-04-22 22:39:32 +0800

    完成了首页的功能开发

--- assets/API/ajaxp.js
+++ assets/API/ajaxp.js
@@ -1,4 +1,17 @@
 // 封装ajax
 $.ajaxPrefilter(function (options) {
   options.url = 'http://www.liulongbin.top:3007' + options.url
+
+  // 统一加入请求头
+  if (options.url.indexOf('/my') !== -1) {
+    options.headers = {
+      Authorization: localStorage.getItem('token') || ''
+    }
+  }
+  options.complete = function (res) {
+    if (res.responseJSON.status === 1) {
+      localStorage.removeItem('token')
+      location.href = '/login.html'
+    }
+  }
 })
---
+++ assets/js/index.js
@@ -0,0 +1,62 @@
+$(function () {
+  var layer = layui.layer
+  // 调用用户数据
+  getUserinfo()
+  // 绑定退出
+  $('#btnExitLogin').on('click', function () {
+    console.log(111);
+    layer.confirm('你确定要退出当前账号？', { icon: 3, title: '提示' }, function (index) {
+      //do something
+      localStorage.removeItem('token')
+      location.href = '/login.html'
+      // 关闭询问
+      layer.close(index);
+    });
+
+
+  })
+
+})
+function getUserinfo() {
+  $.ajax({
+    method: 'GET',
+    url: '/my/userinfo',
+    // 请求头配置
+    // headers: {
+    //   Authorization: localStorage.getItem('token') || ''
+    // },
+    success: function (res) {
+      if (res.status !== 0) {
+        location.href = '/login.html'
+        return layer.msg(res.message)
+
+      }
+      readuserinfo(res.data)
+
+    },
+    // 执行回调
+    complete: function (res) {
+      if (res.responseJSON.status === 1) {
+        localStorage.removeItem('token')
+        location.href = '/login.html'
+      }
+    }
+  })
+}
+function readuserinfo(data) {
+
+  // 渲染用户名
+  let username = data.username || data.username
+  $('#usernametext').html(`欢迎 ${username}`)
+
+  // 渲染用户头像
+  if (data.user_pic !== null) {
+    $('.layui-nav-img').attr('src', data.user_pic).show()
+    $('.defaAut').hide()
+  } else {
+    let pic = data.username.slice(0, 1).toUpperCase()
+    $('.defaAut').html(pic).show()
+    $('.layui-nav-img').hide()
+  }
+
+}
